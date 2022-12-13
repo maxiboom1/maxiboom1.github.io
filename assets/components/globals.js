de6ebf70729668919fd591870bbe219afbd29b35
Alex Samih-zade <galutziogen@gmail.com>
2022-12-13 17:12:54 +0200

    reorder v.1

---
+++ assets/components/globals.js
@@ -0,0 +1,44 @@
+const _SPINNER = `
+<div class="text-center">
+<div class="spinner-grow text-light" role="status" style="width: 20rem; height: 20rem;">
+<span class="visually-hidden">Loading...</span>
+</div>
+</div>`;
+
+const _SPINNER_BTN = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+<span class="visually-hidden">Loading...</span>`;
+
+const _SEARCHFAIL = `<main class="container">
+<div class="bg-light p-5 rounded">
+  <h1>No Luck today :(</h1>
+  <p class="lead">Try harder next time - no one gets ride for free.</p>
+</div>
+</main>`
+
+const _ABOUT = `
+<div class="container mt-4 mb-4 p-3 d-flex justify-content-center">
+        
+<div class="card p-4">
+
+    <div class="image d-flex flex-column justify-content-center align-items-center">
+
+        <button class="btn btn-secondary"> <img src="assets/img/profile.png" height="100" width="100" /></button>
+        <span class="name mt-3">Alex Samih-zade</span> <span class="idd">@Maxiboom1</span>
+        
+        <div class="d-flex flex-row justify-content-center align-items-center mt-3">
+            <span class="number">5 <span class="follow">Happy JS years</span></span> </div>
+        <div class=" d-flex mt-2"> 
+        <button class="btn1 btn-dark" onclick="test()">Show me more</button> 
+        </div>
+        <div class="text mt-3">
+            <span>Alex is a Full Stack Web Developer with an emphasis on complex, challenging JS coding.<br><br>
+            </span>
+        </div>
+        
+        <div class="gap-3 mt-3 icons d-flex flex-row justify-content-center align-items-center">
+            <span><i class="fa fa-github"></i></span> 
+            <span><i class="fa fa-facebook-f"></i></span>
+        </div>            
+    </div>
+</div>
+</div> `;
